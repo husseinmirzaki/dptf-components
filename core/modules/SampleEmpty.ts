f2579659781c81a650916a5f5beae57fe4378632
powerfuldeveloper <husseinmirzaki@gmail.com>
2022-05-26 08:06:50 +0430

    [stage] better looking tabs and more customizations

---
+++ core/modules/SampleEmpty.ts
@@ -0,0 +1,33 @@
+import {Actions, Mutations} from "@/custom/store/enums/StoreEnums";
+import {Module, Action, Mutation, VuexModule} from "vuex-module-decorators";
+
+
+@Module
+export default class AuthModule extends VuexModule {
+    sampleData: Array<string> = [];
+
+    /**
+     * sample
+     */
+    get sampleGetter(): Array<string> {
+        return this.sampleData;
+    }
+
+
+    /**
+     * sample
+     */
+    @Mutation
+    [Mutations.SET_ERROR](data) {
+        this.sampleData = data;
+    }
+
+
+    /**
+     * sample
+     */
+    @Action
+    [Actions.LOGIN](payload) {
+        //
+    }
+}
