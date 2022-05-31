c39765ad0bff4f7cd84e67e131a03e5ca419a923
powerfuldeveloper <husseinmirzaki@gmail.com>
2022-05-31 19:10:11 +0430

    [stage] draft stuff

---
+++ helpers/MiltiTasking.ts
@@ -0,0 +1,29 @@
+import {AxiosResponse} from "axios";
+
+type ToDoTask = () => Promise<void>;
+
+export class TaskManager {
+    private tasks: Array<ToDoTask> = [];
+    private tasksAreRunning = false;
+
+    get hasTask() {
+        return this.tasks.length > 0;
+    }
+
+    set addTask(task: ToDoTask) {
+        this.tasks.push(task);
+        this.runTasks();
+    }
+
+    private async runTasks() {
+        console.log("this.tasksAreRunning", this.tasksAreRunning);
+        if (this.tasksAreRunning) return;
+        this.tasksAreRunning = true;
+        let lastTask = this.tasks.pop();
+        while (lastTask) {
+            await lastTask()
+            lastTask = this.tasks.pop();
+        }
+        this.tasksAreRunning = false;
+    }
+}
