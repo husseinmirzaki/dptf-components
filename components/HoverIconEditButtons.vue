f64cee6c806ab21797f4225b29d5db3a4d30aaa8
MahmoudAbadi <mohsen.gs99@gmail.com>
2022-06-02 19:58:10 +0430

    [Feature] Communication Groups

---
+++ components/HoverIconEditButtons.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="btn w-100 d-flex justify-content-between align-items-center the-nav-buttons pl-1" :class="{active}">
+    <div>
+    <i v-if="iconClass" :class="iconClass" class="fs-4 me-2"></i>
+    <i v-else class="fas fa-envelope-open-text fs-4 me-2"></i>
+    <slot/>
+    <span
+        class="badge position-absolute" :class="{
+          'badge-primary': badgeNumber && (Number(badgeNumber) > 0 && Number(badgeNumber) < 3),
+          'badge-warning': badgeNumber && (Number(badgeNumber) > 2 && Number(badgeNumber) < 6),
+          'badge-danger': badgeNumber && (Number(badgeNumber) > 5),
+        }"
+        v-if="badgeNumber">{{ badgeNumber }}</span>
+
+    <span v-if="badgeText" class="badge position-absolute badge-danger">
+      {{ badgeText }}
+    </span>
+      </div>
+    <div>
+    <button class="btn btn-secondary btn-icon btn-sm" @click="$emit('delete', $event)"><i class="fa fa-trash"></i></button>
+    <button class="btn btn-secondary btn-icon btn-sm" @click="$emit('update', $event)"><i class="fa fa-edit"></i></button></div>
+  </div>
+
+</template>
+<script>
+export default {
+  props: ["active", "badgeNumber", "iconClass", "badgeText"]
+}
+</script>
+<style lang="scss">
+.the-nav-buttons {
+
+  font-size: 12px;
+
+  position: relative;
+  margin-top: 5px;
+  padding-left: 35px !important;
+  padding-right: 5px !important;
+
+  &.active {
+    background-color: rgba(70, 70, 248, 0.22);
+  }
+
+  &:not(.active):hover {
+    background-color: rgba(56, 56, 124, 0.22);
+  }
+
+  span {
+    left: 6px;
+    top: 50% !important;
+    transform: translateY(-50%);
+    font-size: 10px;
+    padding: 6px;
+  }
+}
+</style>
