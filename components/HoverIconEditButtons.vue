c30ab4287200dd82cf1766471a262fbdf504af8b
Mohsen Mahmoud Abadi <mohsen.gs99@gmail.com>
2022-06-05 12:33:57 +0430

    [Feature] Communication Group Tables

--- components/HoverIconEditButtons.vue
+++ components/HoverIconEditButtons.vue
@@ -1,11 +1,14 @@
 <template>
-  <div href="#" class="btn w-100 d-flex justify-content-start align-items-center the-nav-buttons" :class="{active}">
+  <div href="#" class="btn w-100 d-flex justify-content-between align-items-center the-nav-buttons" :class="{active}">
+    <div class="col-8">
     <i v-if="iconClass" :class="iconClass" class="fs-4 me-2"></i>
     <i v-else class="fas fa-envelope-open-text fs-4 me-2"></i>
     <slot/>
-
-    <button class="btn btn-icon btn-secondary" @click="$emit('update', $event)"><i class="fa fa-edit"></i></button>
-    <button class="btn btn-icon btn-secondary" @click="$emit('delete', $event)"><i class="fa fa-delete"></i></button>
+</div>
+    <div class="col-4">
+    <button class="btn btn-icon btn-secondary " @click="$emit('update', $event)"><i class="fa fa-edit"></i></button>
+    <button class="btn btn-icon btn-secondary ml-2" @click="$emit('delete', $event)"><i class="fa fa-trash"></i></button>
+      </div>
   </div>
 
 </template>
