2b3ff3bf2482f8109917d0f150a0e318a4e98c4d
powerfuldeveloper <husseinmirzaki@gmail.com>
2022-06-20 18:51:22 +0430

    [stage] new editor requirements

---
+++ components/ck-editor-plugins/CkEditorReferencePlugin.js
@@ -0,0 +1,124 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import {toWidget, toWidgetEditable} from '@ckeditor/ckeditor5-widget/src/utils';
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+
+export default class CkEditorReferencePlugin extends Plugin {
+
+    init() {
+        this._emitter = this.editor.config.emitter;
+        this._addButton();
+        this._defineSchema();
+        this._defineConverters();
+    }
+
+    _addButton() {
+        this.editor.ui.componentFactory.add('insertReference', locale => {
+            const view = new ButtonView(locale);
+
+            view.set({
+                label: 'اضافه کردن عطف',
+                tooltip: true,
+                withText: true,
+            });
+
+            view.on('execute', () => {
+                this.editor.config._config.emitter('addReference', this);
+            });
+
+            return view;
+        });
+    }
+
+    _defineSchema() {
+        const schema = this.editor.model.schema;
+        schema.register('reference', {
+            // avoid easy delets
+            isInline: true,
+            isObject: true,
+            // Allow in places where other blocks are allowed (e.g. directly in the root).
+            allowWhere: '$inlineObject',
+            allowContentOf: '$block',
+            allowAttributesOf: '$block',
+            allowAttributes: 'letterId',
+        });
+
+        schema.register( 'letter-number', {
+            // Cannot be split or left by the caret.
+            isLimit: true,
+            isInline: true,
+
+            allowIn: 'reference',
+
+            // Allow content which is allowed in blocks (i.e. text with attributes).
+            allowWhere: '$inlineObject',
+            allowContentOf: '$block',
+            allowAttributesOf: '$block',
+        } );
+    }
+
+    _defineConverters() {                                                      // ADDED
+        const conversion = this.editor.conversion;
+
+        // this will convert sth called reference to a span
+        conversion.for('upcast').elementToElement({
+            model: 'reference',
+            view: {
+                name: 'span',
+                classes: 'letter-reference',
+            }
+        });
+
+        conversion.for('dataDowncast').elementToElement({
+            model: 'reference',
+            view: {
+                name: 'span',
+                classes: 'letter-reference',
+            }
+        });
+
+        conversion.for('editingDowncast').elementToElement({
+            model: 'reference',
+            view: (modelElement, {writer: viewWriter}) => {
+                const span = viewWriter.createContainerElement('span', {class: 'letter-reference'});
+
+                return toWidget(span, viewWriter, {label: 'عطف به یک نامه'});
+            }
+        });
+
+        // this will convert sth called reference to a span
+        conversion.for('upcast').elementToElement({
+            model: 'letter-number',
+            view: {
+                name: 'u',
+                classes: 'letter-reference-underline',
+            }
+        });
+
+        conversion.for('dataDowncast').elementToElement({
+            model: 'letter-number',
+            view: {
+                name: 'u',
+                classes: 'letter-reference-underline',
+            }
+        });
+
+        conversion.for('editingDowncast').elementToElement({
+            model: 'letter-number',
+            view: (modelElement, {writer: viewWriter}) => {
+                const span = viewWriter.createContainerElement('u', {class: 'letter-reference-underline'});
+
+                return toWidget(span, viewWriter, {label: 'شماره نامه'});
+            }
+        });
+
+        conversion.for('upcast').attributeToAttribute({
+            model: 'letterId',
+            view: 'letter-id'
+        })
+
+        conversion.for('dataDowncast').attributeToAttribute({
+            model: 'letterId',
+            view: 'letter-id'
+        })
+    }
+}
--- components/document/WordLikeEditor.vue
+++ components/document/WordLikeEditor.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="row">
     <div class="col-12">
-      <CkEditor :editor="editorClass" @ready="onReady" :config="editorConfig"/>
+      <CkEditor :editor="editorClass" @ready="onReady" :config="editorConfig"
+                @update:modelValue="$emit('update:modelValue', $event)" :modelValue="modelValue"/>
     </div>
   </div>
 </template>
 <style lang="scss">
 .ck-editor {
   .ck-toolbar, .ck-content {
+  min-height: 50px;
     border-radius: 0 !important;
   }
 }
@@ -19,29 +21,77 @@ import CKEditor from '@ckeditor/ckeditor5-vue';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
+
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/table';
+
+import ParagraphButtonUI from '@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import CkEditorReferencePlugin from "@/custom/components/ck-editor-plugins/CkEditorReferencePlugin.js";
 
 export default {
   components: {
     CkEditor: CKEditor.component,
   },
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
   data() {
     return {
+      editor: null,
       editorClass: ClassicEditor,
       editorConfig: {
+        emitter: this.$emit,
+        alignment: {
+          options: ['left', 'right', 'center', 'justify']
+        },
         language: 'fa',
         plugins: [
           EssentialsPlugin,
+
+          TableToolbar,
           BoldPlugin,
           ItalicPlugin,
-          ParagraphPlugin
+          Underline,
+          Strikethrough,
+          Superscript,
+          Subscript,
+
+          PasteFromOffice,
+          Indent,
+          Alignment,
+          List,
+          ParagraphPlugin,
+          ParagraphButtonUI,
+          Heading,
+          CkEditorReferencePlugin,
         ],
         toolbar: {
+          table: {
+            contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+          },
           items: [
+            'alignment', '|',
+            'bulletedList',
+            'numberedList',
+            'insertTable',
+            'indent', '|',
+            'insertReference',
             'bold',
-            'italic',
+            'superscript',
+            'subscript',
+            'strikethrough',
+            'underline',
+            'italic', '|',
             'undo',
             'redo'
           ]
@@ -51,11 +101,13 @@ export default {
   },
   methods: {
     onReady(editor) {
+      this.editor = editor;
+      this.$emit('ready', this.editor);
       // Insert the toolbar before the editable area.
-      editor.ui.getEditableElement().parentElement.parentElement.insertBefore(
-          editor.ui.view.toolbar.element,
-          editor.ui.getEditableElement()
-      );
+      // editor.ui.getEditableElement().parentElement.parentElement.insertBefore(
+      //     editor.ui.view.toolbar.element,
+      //     editor.ui.getEditableElement()
+      // );
     }
   }
 }
