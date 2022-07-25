384b38b3b477e858c9450ed086058683d38c6fd5
MahmoudAbadi <mohsen.gs99@gmail.com>
2022-07-25 09:46:47 +0430

    [Feature] Project Payments Page Is Done

---
+++ components/table/tbody/TableTDCurrency.vue
@@ -0,0 +1,31 @@
+<template>
+  <table-t-d style="direction: ltr;">
+    {{ converted }}
+  </table-t-d>
+</template>
+<script lang="ts">
+import {computed, defineComponent, toRef} from "vue";
+import TableTD from "@/custom/components/table/tbody/TableTD.vue";
+import {gregorianIOSToPersianDate, gregorianToJalali} from "@/custom/components/DateUtils";
+
+export default defineComponent({
+  components: {TableTD},
+  props: ['data'],
+  setup(props) {
+    const data = toRef(props, 'data');
+
+    const converted = computed(() => {
+      if (data.value) {
+        const price = Number(data.value)
+        const iranLocale = Intl.NumberFormat('fa-IR')
+        return iranLocale.format(price)
+      }
+      return '';
+    });
+
+    return {
+      converted,
+    }
+  }
+});
+</script>
