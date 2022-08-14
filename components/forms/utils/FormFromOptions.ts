5e702ebb8799863807c4e8be43521ed223f9c6ae
powerfuldeveloper <husseinmirzaki@gmail.com>
2022-08-14 14:21:58 +0430

    [stage] tab stuff

--- components/forms/utils/FormFromOptions.ts
+++ components/forms/utils/FormFromOptions.ts
@@ -1,3 +1,15 @@
 export function formFromOptions(options: Record<string, any>) {
-    //
+    return Object.values(options)
+        .filter((d: any) => d['label'] != 'ID')
+        .map((d: any) => {
+            switch (d['type']) {
+                case 'integer':
+                    d.field_type = 'number';
+                    break;
+                case 'string':
+                    d.field_type = 'text';
+                    break;
+            }
+            return d;
+        })
 }
