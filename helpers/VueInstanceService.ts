5323c1740c16514c6fc37f820703e45dd532cbce
Powerfuldeveloper <h.mirzaki@kashandevops.ir>
2022-08-28 13:25:52 +0430

    [stage] sweet

--- helpers/VueInstanceService.ts
+++ helpers/VueInstanceService.ts
@@ -3,7 +3,6 @@ import * as Yup from "yup";
 import {addMethod, setLocale} from "yup";
 import {Router} from "vue-router";
 import {Actions} from "@/store/enums/StoreEnums";
-import {SweetAlertOptions} from "sweetalert2";
 
 export default class VueInstanceService {
     public static pageIsLoading = ref(false);
@@ -101,7 +100,7 @@ export default class VueInstanceService {
         });
     }
 
-    public static swal(config: SweetAlertOptions | string) {
+    public static swal(config: any) {
         return this.vue.config.globalProperties["$swal"](config);
     }
 
