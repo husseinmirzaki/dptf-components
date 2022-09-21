099128c98ce457ea866fae25a277e29db6b2df73
Powerfuldeveloper <h.mirzaki@kashandevops.ir>
2022-09-21 11:09:50 +0430

    [stage] removed not required validation from read only forms

--- components/forms/StateManager.vue
+++ components/forms/StateManager.vue
@@ -5,7 +5,9 @@ export default {
   props: ['states'],
   setup(props, context) {
     const activeState = ref(props.states[0]);
-
+    context.expose({
+      activeState
+    })
     return () => {
       return context.slots[activeState.value]()
     }
