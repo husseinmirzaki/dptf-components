68aa5fed32c42f20a5f286dc71d9ec77696be1ff
MahmoudAbadi <mohsen.gs99@gmail.com>
2022-09-22 17:44:03 +0330

    mohsen

--- components/FieldComponentCurrency.js
+++ components/FieldComponentCurrency.js
@@ -39,14 +39,14 @@ export function formatCurrency(input, blur) {
         right_side = right_side.substring(0, 2);
 
         // join number by .
-        input_val = left_side + " میلیارد تومان";
+        input_val = left_side + " ";
 
     } else {
         // no decimal entered
         // add commas to number
         // remove all non-digits
         input_val = formatNumber(input_val);
-        input_val = input_val + " میلیارد تومان";
+        input_val = input_val + " ";
         // send updated string to input
         input.value = input_val;
 
