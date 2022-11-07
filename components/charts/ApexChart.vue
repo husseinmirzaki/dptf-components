5ba31a707ee14ad05dd296ce4a08454524a12dab
powerfuldeveloper <husseinmirzaki@gmail.com>
2022-11-07 12:16:20 +0330

    [stage] apex

---
+++ components/charts/ApexChart.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import VueApexCharts from 'vue3-apexcharts';
+import {h} from "vue";
+
+export default {
+  setup() {
+    return () => {
+      return h(VueApexCharts as any,
+          {
+            class: 'dptf-custom-vue-apex-charts'
+          }
+      )
+    }
+  }
+}
+</script>
+<style lang="scss">
+.dptf-custom-vue-apex-charts {
+  direction: ltr;
+}
+</style>
