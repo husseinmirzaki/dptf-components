d28760338834408d1b56ea418c91616006a2e969
Mojtaba Ghasemi <mghasemi13786@gmail.com>
2022-12-01 17:46:40 +0330

    [fixbug] menu item show to user algorithm

--- components/table/tbody/TableTDUserMulti.vue
+++ components/table/tbody/TableTDUserMulti.vue
@@ -5,7 +5,7 @@
       @click="
         $viewerApi({
           images: firstThree.map((e) =>
-            e.avatar ? e.avatar : 'media/avatars/blank2.jpg'
+            e.avatar ? e.avatar : 'media/avatars/blank.png'
           ),
         })
       "
@@ -17,7 +17,7 @@
         title="User profile"
       >
         <img
-          :src="user.avatar ? user.avatar : 'media/avatars/blank2.jpg'"
+          :src="user.avatar ? user.avatar : 'media/avatars/blank.png'"
           alt="metronic"
         />
       </div>
