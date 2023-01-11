22ea6e8ac62ab8674d3f53b249c07d9d48b717bc
Soheyl <s.mehtari@tadbirserver.ir>
2023-01-11 17:28:39 +0330

    today

--- services/UserApiService.ts
+++ services/UserApiService.ts
@@ -131,9 +131,4 @@ export class UserApiService extends ApiService {
   public static getCurrentUserRoles() {
     return ApiService.get(`${this.baseUrl}get_user_possible_roles/`);
   }
-
-  public static getMotamedList(){
-    console.log('aa',ApiService.list(`${this.baseUrl}motamed_list/`))
-    return ApiService.list(`${this.baseUrl}motamed_list/`);
-  }
 }
