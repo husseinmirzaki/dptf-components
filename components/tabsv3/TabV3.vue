0d2e12f0380dd186a058e57c02962f509cef1118
MahmoudAbadi <mohsen.gs99@gmail.com>
2023-01-18 17:42:36 +0330

    temp

---
+++ components/tabsv3/TabV3.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-show="isActive">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "TabV3",
+  props: {
+    title: {
+      type: String,
+      default: 'Tab'
+    }
+  },
+  setup() {
+    const isActive = ref(true)
+    return {
+      isActive
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/tabsv3/TabsV3.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul class="tabs__header">
+      <li v-for="tab in tabs" :key="tab.title">{{ tab.title }}</li>
+    </ul>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import {onMounted, ref} from "vue";
+
+export default {
+  name: "TabsV3",
+  setup(props, context) {
+    const selectedIndex = ref(0)
+    const tabs = ref([])
+
+    onMounted(()=>{
+      tabs.value = context.children()
+    })
+
+    return{
+      selectedIndex,
+      tabs,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
