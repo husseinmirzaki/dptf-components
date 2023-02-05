fe99f724697bd5538b902903c149938d95e33eb2
MahmoudAbadi <mohsen.gs99@gmail.com>
2023-02-05 13:03:31 +0330

    [Feature] Families

---
+++ components/table/tbody/TableTDTags.vue
@@ -0,0 +1,20 @@
+<template>
+  <table-t-d style="direction: ltr">
+    <span v-for="(item, i) in data" :key="i" :class="`badge badge-${item.color} mx-1`">{{ item.title }}</span>
+  </table-t-d>
+</template>
+<script lang="ts">
+import { defineComponent, toRef } from "vue";
+import TableTD from "@/custom/components/table/tbody/TableTD.vue";
+import { VueInstanceService } from "@/Defaults";
+
+export default defineComponent({
+  components: { TableTD },
+  props: ["data"],
+  setup() {
+    return {
+      VueInstanceService,
+    };
+  },
+});
+</script>
